e2698ea5dab1bf7477a9eca3fd2d157fce19970f
yuyaebean <yuyaebean@gmail.com>
2019-10-08 23:32:36 +0900

    5-1. Signup & Signin

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'portfolio.apps.PortfolioConfig',
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- secondproject/urls.py
+++ secondproject/urls.py
@@ -21,10 +21,8 @@ from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', views.home, name='home'),
-
     path('blog/', include('blog.urls')),
-
     path('portfolio/', include('portfolio.urls')),
+    path('account/', include('account.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
